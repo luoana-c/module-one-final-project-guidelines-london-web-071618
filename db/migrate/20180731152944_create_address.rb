2fdd9f6e875b7e8eea8223efbda827bbeff2bd46
github username <github email address>
2018-07-31 17:32:35 +0100

    added 2 tables: address and bike station status

--- db/migrate/20180731152944_create_address.rb
+++ db/migrate/20180731152944_create_address.rb
@@ -1,4 +1,9 @@
 class CreateAddress < ActiveRecord::Migration[5.0]
   def change
+    create_table :addresses do |t|
+     t.string :address
+     t.float :lat
+     t.float :long
+   end
   end
 end
--- db/migrate/20180731153010_create_bike_station_status.rb
+++ db/migrate/20180731153010_create_bike_station_status.rb
@@ -1,4 +1,8 @@
 class CreateBikeStationStatus < ActiveRecord::Migration[5.0]
   def change
+    create_table :bike_stations_status do |t|
+     t.integer :num_bikes_available
+     t.integer :num_empty_docks
+   end
   end
 end
